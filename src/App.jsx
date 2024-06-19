4c3c43b126c9865698f69211a19da01dc3b7a12d
Abhishek <abhishekbhikule76@gmail.com>
2024-06-19 20:35:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Swiper core and required modules
+
+import SwiperComponent from "./SwiperComponent";
+
+function App() {
+  return (
+    <>
+      <SwiperComponent />
+    </>
+  );
+}
+
+export default App;
